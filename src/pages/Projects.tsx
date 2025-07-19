146ace42ee592314ac4c2885bee419ba7f16aaee
Lochan patra <143163989+lochanpatra@users.noreply.github.com>
2025-07-19 15:23:16 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -200,45 +200,45 @@ const Projects = () => {
         <ProjectCard
           title="NYC Yellow Taxi Trip Analysis Dashboard"
           description="A scalable dashboard that leverages PySpark to analyze NYC Yellow Taxi trip data for trends in trip volume, fare amounts, and more."
-          image="/images/nyc.png"
+          image="/nyc.png"
           github="https://github.com/lochanpatra/bigdata"
           webLink="https://lochannyctrip.streamlit.app/"
         />
         <ProjectCard
           title="Air Quality Prediction Dashboard"
           description="An interactive Streamlit web app that visualizes air quality data and predicts the Air Quality Index (AQI) using a trained XGBoost model."
-          image="/images/aqi1.png"
+          image="/aqi1.png"
           github="https://github.com/lochanpatra/Air_Quality_Vis"
           webLink="https://lochanairquality.streamlit.app/"
         />
         <ProjectCard
           title="U-Net for Road Segmentation in Mumbai using PyTorch"
           description="U-Net architecture in PyTorch for semantic segmentation of roads from satellite imagery."
-          image="/images/mumbai.png"
+          image="/mumbai.png"
           github="https://github.com/lochanpatra/U-Net_pytorch"
         />
         <ProjectCard
           title="Canberra Zoning Compliance Checker"
           description="A Streamlit app using OpenAI GPT-4 Vision to analyze architectural site plans and verify zoning compliance."
-          image="/images/showcase1.png"
+          image="/showcase1.png"
           github="https://github.com/lochanpatra/zonal-compliance-checker"
         />
         <ProjectCard
           title="GeoIntel-AI: GIS + OpenAI Spatial Analysis App"
           description="Interactive app combining GIS analysis with GPT-4 summaries. Users upload shapefiles and get analysis + natural language descriptions."
-          image="/images/showcase1.png"
+          image="/showcase1.png"
           github="https://github.com/lochanpatra/OpenAI-Spatial-Analysis-"
         />
         <ProjectCard
           title="Real Estate Analyzer"
           description="A Streamlit app that scores property images, forecasts prices using Prophet, and visualizes comps."
-          image="/images/aqi.png"
+          image="/aqi.png"
           github="https://github.com/lochanpatra/real_estate_analyzer"
         />
         <ProjectCard
           title="Agridashboard"
           description="A web app using Open Meteo API to forecast temperature, humidity, and rainfall over 12 hours."
-          image="/images/aqi.png"
+          image="/aqi.png"
           webLink="https://agridashbord.netlify.app/"
         />
       </div>
